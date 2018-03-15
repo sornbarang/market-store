4eb8092b10d1c2b09b950fb01740c74334eb2c08
vitou <pitouchhay65@gmail.com>
2018-03-15 22:48:22 +0700

    home page

--- resources/views/page.blade.php
+++ resources/views/page.blade.php
@@ -2,14 +2,14 @@
 @section('content')
 <div class="container-fluid" style="margin:auto;text-align:center;">
     <figure class="figure" style="margin:0;">
-        <img src="{{asset('/')}}imgs/logo.svg" class="figure-img img-fluid rounded" alt="A generic square placeholder image with rounded corners in a figure.">
+        <img src="{{asset('/')}}imgs/logo.png" class="figure-img img-fluid rounded-circle" alt="A generic square placeholder image with rounded corners in a figure.">
     </figure>
-    <div class="d-flex justify-content-center bd-highlight mb-3"  style="margin-bottom:0 !important;">
-    <div class="btn-group" role="group" aria-label="Basic example">
-    <button type="button" class="btn btn-primary btn-sm">F To F</button>
-<button type="button" class="btn btn-secondary btn-sm">C To C</button>
-    </div>
-</div>
+    <br/> 
+    <br/> 
+    <h3 class="text-center">Choose</h3> 
+    <br/>
+    <button type="button" class="btn btn-outline-primary btn-lg">F To F</button>
+    <button type="button" class="btn btn-outline-info btn-lg">C To C</button> 
 </div>
 
 
