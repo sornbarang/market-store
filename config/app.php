2feba9424022dc249ac10c30bfe897e85f94bc4a
Barang CK <sornbarang@gmail.com>
2018-05-26 10:59:36 +0700

    add laravelcollective/html package

--- config/app.php
+++ config/app.php
@@ -153,6 +153,7 @@ return [
         Spatie\Permission\PermissionServiceProvider::class,
         Dimsav\Translatable\TranslatableServiceProvider::class,
         'Baum\Providers\BaumServiceProvider',
+        Collective\Html\HtmlServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -211,6 +212,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'Form' => Collective\Html\FormFacade::class,
+        'Html' => Collective\Html\HtmlFacade::class,
         // 'AdvMng'    => Adumskis\LaravelAdvert\AdvertFacade::class,
     ],
 
