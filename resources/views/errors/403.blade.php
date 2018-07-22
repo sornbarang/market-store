fb5180b775443c426ca883e49e0842508c8ddaec
barang ck <sornbarang@gmail.com>
2018-07-22 08:35:24 +0700

    list report

---
+++ resources/views/errors/403.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.app-layout')
+@section('content')
+
+    <!--================================
+            START 403 AREA
+    =================================-->
+    <section class="four_o_four_area section--padding2">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6 offset-md-3 text-center">
+                    <div class="not_found">
+                        <h3>ACCESS DENIED</h3>
+                        <a href="{{ URL('/') }}" class="btn btn--round btn--default">Back to Home</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--================================
+            END 404 AREA
+    =================================-->
+@stop
