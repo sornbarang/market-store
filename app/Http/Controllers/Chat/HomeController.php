8ecf46d8ee50fe36bfd4e83781310272d218a7cb
vitou <pitouchhay65@gmail.com>
2018-09-23 10:12:55 +0700

    check if exist session

--- app/Http/Controllers/Chat/HomeController.php
+++ app/Http/Controllers/Chat/HomeController.php
@@ -30,12 +30,12 @@ class HomeController extends Controller
     }
     public function createSession(Request $request){ 
         // must be check exist session record 
-        // $chkSession = Session::where('user1_id',auth()->id())->where('user2_id',$request->friend_id)->first();
-        // if(!$chkSession){
-        //     Session::create(['user1_id' => auth()->id(), 'user2_id' => $request->friend_id]);
-        // }
+        $chkSession = Session::where('user1_id',auth()->id())->where('user2_id',$request->friend_id)->first();
+        if(!$chkSession){
             Session::create(['user1_id' => auth()->id(), 'user2_id' => $request->friend_id]);
-
+        }
+        // Session::create(['user1_id' => auth()->id(), 'user2_id' => $request->friend_id]);
+        
         // get active user mean select user to comunicate
         $checkSessionUsrs = UserResource::collection(User::where('id', '!=', auth()->id())->where('id',$request->friend_id)->get());
         return $checkSessionUsrs;
