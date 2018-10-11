726a690c92a65adbe3bbcfc04b018d15d8f94c2b
vitou <pitouchhay65@gmail.com>
2018-10-11 11:54:51 +0700

    fix error no image

--- resources/views/c2c/page/index.blade.php
+++ resources/views/c2c/page/index.blade.php
@@ -93,7 +93,7 @@
                                     @foreach($cats['product'] as $lpk => $getprops)
                                         @php 
                                             $avatar='';  
-                                            $media = $getprops->user->profile->getMedia(); 
+                                            $media = null !== $getprops->user->profile->getMedia()?$getprops->user->profile->getMedia():[]; 
                                             foreach($media as $val){   
                                                 if($getprops->user->profile->avatar == $val->id){
                                                     $avatar=$val->id.'/'.$val->file_name;  
