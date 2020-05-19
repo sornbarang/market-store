0d75aaefdeddd23abb6087a1771b3f5d975e5737
chhay vitou <pitouchhay65@gmail.com>
2020-05-19 21:07:44 +0700

    push

--- resources/views/layouts/chat-layout.blade.php
+++ resources/views/layouts/chat-layout.blade.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html>
 <html lang="{{ app()->getLocale() }}">
-
 <head>
     <meta charset="utf-8">
+    <meta http-equiv="Content-Security-Policy" content="script-src 'self' 'unsafe-eval' 'unsafe-inline' ">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <script>
